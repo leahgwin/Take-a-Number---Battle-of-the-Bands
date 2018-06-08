fd686a69e15e109298abbbdfe7369be3d3f75813
leahgwin <leahgwin@gmail.com>
2018-06-08 13:43:28 -0500

    fixed the undefined console log

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const takeNumber = function (bandName) {
 }
 
 const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
+// console.log(scum)  // This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+// console.log(under)  // This should print "2. Underdogs" in the console
